5ed93448b187ae7dd10a7f7acafc85bba67e6c30
iron <>
2023-07-17 16:51:51 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    // reactStrictMode: true,
+    // images: {
+    //     unoptimized: true
+    // }
+
+}
 
 module.exports = nextConfig
